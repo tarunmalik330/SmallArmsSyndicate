48431b8800d1a91216f811b50387f9e1c4848d45
sakshi pareek <sakshipareek505@gmail.com>
2023-09-29 16:44:34 +0530

    hero-section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
-function name(params) {
+// ----------------------------navbar----------------------------
 
+let label = document.querySelector("#label") 
+let navbox = document.querySelector(".navbox") 
+label.addEventListener("click" , function (){
+    navbox.classList.toggle("left-0")
+})
+function showNav() {
+    document.body.classList.toggle('overflow-hidden')
+    document.getElementById("label").classList.toggle("cross_icon");
 }
