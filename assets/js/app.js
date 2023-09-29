9575581c2c28f78f248598a3e89c341f1acd55a1
tarunmalik330 <maliktarun2005@gmail.com>
2023-09-29 22:11:52 +0530

    start new section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,25 @@
 // ----------------------------navbar----------------------------
 
-let label = document.querySelector("#label") 
-let navbox = document.querySelector(".navbox") 
-label.addEventListener("click" , function (){
+let label = document.querySelector("#label")
+let navbox = document.querySelector(".navbox")
+label.addEventListener("click", function () {
     navbox.classList.toggle("left-0")
 })
 function showNav() {
     document.body.classList.toggle('overflow-hidden')
     document.getElementById("label").classList.toggle("cross_icon");
 }
+// scroll
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 100) {
+//             $('#scroll').fadeIn();
+//         } else {
+//             $('#scroll').fadeOut();
+//         }
+//     });
+//     $('#scroll').click(function () {
+//         $("html, body").animate({ scrollTop: 0 }, 500);
+//         return false;
+//     });
+// });
