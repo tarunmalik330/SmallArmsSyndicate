e4539814dde877201b721eac00a3cfeb7904d50e
tarunmalik330 <maliktarun2005@gmail.com>
2023-09-30 11:11:51 +0530

    complete section Royalty Stash

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,17 +10,3 @@ function showNav() {
     document.body.classList.toggle('overflow-hidden')
     document.getElementById("label").classList.toggle("cross_icon");
 }
-// scroll
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() > 100) {
-//             $('#scroll').fadeIn();
-//         } else {
-//             $('#scroll').fadeOut();
-//         }
-//     });
-//     $('#scroll').click(function () {
-//         $("html, body").animate({ scrollTop: 0 }, 500);
-//         return false;
-//     });
-// });
