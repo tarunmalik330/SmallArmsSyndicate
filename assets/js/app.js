376f805d60b31d6a7b75ade49fce178ec44788be
sakshi pareek <sakshipareek505@gmail.com>
2023-09-30 16:11:23 +0530

    team seaction

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,38 @@ function showNav() {
     document.body.classList.toggle('overflow-hidden')
     document.getElementById("label").classList.toggle("cross_icon");
 }
+// -----------------------team-seaction------------------
+$('.team_slider').slick({
+    dots: false,
+    arrows:false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2.4,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
