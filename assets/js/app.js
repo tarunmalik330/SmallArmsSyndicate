20b1c45a74afefff32f6c0dfd746d5d8f593c6cb
sakshi pareek <sakshipareek505@gmail.com>
2023-10-01 17:41:11 +0530

    team-seaction

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,57 @@
-// ----------------------------navbar----------------------------
+// -----------------------------preloader---------------------------
+setTimeout(() => {
+  const box = document.getElementById("box");
 
+  box.style.display = "none";
+
+  window.scrollTo(0, 0);
+
+  document.body.classList.remove("overflow-hidden");
+}, 2000);
+
+// ----------------------------navbar----------------------------
 let label = document.querySelector("#label")
 let navbox = document.querySelector(".navbox")
 let nav_bg = document.querySelector(".nav_bg")
 label.addEventListener("click", function () {
-    navbox.classList.toggle("right-0");
+  navbox.classList.toggle("right-0");
 })
 function showNav() {
-    document.body.classList.toggle('overflow-hidden')
-    document.getElementById("label").classList.toggle("cross_icon");
+  document.body.classList.toggle('overflow-hidden')
+  document.getElementById("label").classList.toggle("cross_icon");
 }
 // -----------------------team-seaction------------------
 $('.team_slider').slick({
-    dots: false,
-    arrows:false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2.4,
-          slidesToScroll: 1,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2.4,
+        slidesToScroll: 1,
+        infinite: true,
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
